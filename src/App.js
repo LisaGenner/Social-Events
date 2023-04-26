a2aa66bd68f5c34adfacdc2e03dab59ec3d4ed8a
Lisa Genner <116294867+LisaGenner@users.noreply.github.com>
2023-04-26 18:12:22 -0500

    4.4

---
+++ src/App.js
@@ -0,0 +1,19 @@
+// src/App.js
+
+import React, { Component } from 'react';
+import './App.css';
+import EventList from './EventList';
+import CitySearch from './CitySearch';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <CitySearch />
+        <EventList />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/Event.js
@@ -0,0 +1,10 @@
+// src/Event.js
+
+import React, { Component } from "react";
+
+class Event extends Component {
+  render() {
+    return <div></div>;
+  }
+}
+export default Event;
