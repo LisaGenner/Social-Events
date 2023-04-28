c135a38b0b1e19d6e3ae2973aee64297633e2473
Lisa Genner <116294867+LisaGenner@users.noreply.github.com>
2023-04-28 11:58:18 -0500

    4.4 units tests

--- src/api.js
+++ src/api.js
@@ -7,7 +7,7 @@
  * The Set will remove all duplicates from the array.
  */
 export const extractLocations = (events) => {
-    var extractLocations = events.map((event) => event.location);
-    var locations = [...new Set(extractLocations)];
-    return locations;
-  };
+  var extractLocations = events.map((event) => event.location);
+  var locations = [...new Set(extractLocations)];
+  return locations;
+};
