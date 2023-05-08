3ea1c929fc7725b8bf47da74dabcd6977f557aec
Lisa Genner <116294867+LisaGenner@users.noreply.github.com>
2023-05-08 10:34:27 -0500

    test commit

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 /**
  *
  * @param {*} events:
- * The following function should be in the “api.js” file.
+ * the following function should be in the “api.js” file.
  * This function takes an events array, then uses map to create a new array with only locations.
  * It will also remove all duplicates by creating another new array using the spread operator and spreading a Set.
  * The Set will remove all duplicates from the array.
