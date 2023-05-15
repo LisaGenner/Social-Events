b932e725f7c825cbc1c6ef2aeba7c9705854f182
Lisa Genner <116294867+LisaGenner@users.noreply.github.com>
2023-05-15 14:27:48 -0500

    4.9 PWS set up

--- src/index.js
+++ src/index.js
@@ -1,21 +1,25 @@
 import React from "react";
-import { render } from "react-dom";
+import ReactDOM from 'react-dom';
+// import { render } from "react-dom";
 import "./index.css";
 import App from "./App";
 import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 import reportWebVitals from "./reportWebVitals";
 import * as atatus from 'atatus-js';
-atatus.config('a83f725ad20c48b0a92ac205f28d315d').install();
 
-const root = document.getElementById("root");
-render(<App />, root);
+
+// const root = document.getElementById("root");
+// render(<App />, root);
+ReactDOM.render(<App />, document.getElementById('root')); 
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
-serviceWorkerRegistration.unregister();
+serviceWorkerRegistration.register();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+atatus.config('a83f725ad20c48b0a92ac205f28d315d').install();
