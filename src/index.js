ab5577d4fb7c2a170b0d0724c3cd1926f2680e30
Lisa Genner <116294867+LisaGenner@users.noreply.github.com>
2023-05-16 09:36:05 -0500

    4.9 try to fix service worker error

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom';
 // import { render } from "react-dom";
 import "./index.css";
 import App from "./App";
-// import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
-import * as serviceWorker from './serviceWorker';
+ import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
+// import * as serviceWorker from './serviceWorker';
 import reportWebVitals from "./reportWebVitals";
 import * as atatus from 'atatus-js';
 
@@ -16,8 +16,8 @@ ReactDOM.render(<App />, document.getElementById('root'));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
-//serviceWorkerRegistration.register();
-serviceWorker.register();
+serviceWorkerRegistration.register();
+// serviceWorker.register();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
